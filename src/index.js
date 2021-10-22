bf3816f6e2f2abf4c504a83dc68e7fe2f174e767
Nick Carducci <nmcarducci@gmail.com>
2021-10-22 13:36:27 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import { StrictMode } from "react";
+import React from "react";
+//every page, required for react-scripts, even without using any React attributes
 import ReactDOM from "react-dom";
 
-import App from "./App";
+function App() {
+  return (
+    <div
+      style={{
+        width: "100%"
+      }}
+    >
+      In the back of my mind,” like imagine evolution implied from location of
+      RAM possibly literally actually in frontal lobe
+    </div>
+  );
+}
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
