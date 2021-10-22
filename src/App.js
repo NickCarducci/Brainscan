63ed042475b74ad6ef83b3b014f354f69b523b2f
Nick <nmcarducci@gmail.com>
2021-10-22 13:24:28 -0400

    ok

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div
+      style={{
+        width: "100%"
+      }}
+    >
+      In the back of my mind,” like imagine evolution implied from location of
+      RAM possibly literally actually in frontal lobe
+    </div>
+  );
+}
